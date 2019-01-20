c3e47b57dd60dca48d0a88cbfec851c2d4da7030
rat395 <rat395@ymail.com>
2019-01-20 02:59:20 -0800

    made application be debug mode

--- application.py
+++ application.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+app.config.update(
+    TESTING=True,
+    DEBUG=True
+    )
+
 @app.route("/")
 def hello():
     return render_template('base.html')
